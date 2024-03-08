33760caef220c1d87c2b897826d65243a0c4ff52
Timaisneh <olagunjumichael624@gmail.com>
2024-03-08 03:27:04 -0500

    changes for assignment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,6 +35,8 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make ARCH="${ARCH}" CROSS_COMPILE="${CROSS_COMPILE}" defconfig
+    make ARCH="${ARCH}" CROSS_COMPILE="${CROSS_COMPILE}" -j$(nproc)
 fi
 
 echo "Adding the Image in outdir"
@@ -48,6 +50,9 @@ then
 fi
 
 # TODO: Create necessary base directories
+echo "Creating the staging directory for the root filesystem"
+sudo mkdir -p "${OUTDIR}/rootfs/{bin,dev,etc,home,lib,proc,sbin,sys,tmp,usr,var}"
+sudo mkdir -p "${OUTDIR}/rootfs/home"
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +61,48 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make ARCH="${ARCH}" CROSS_COMPILE="${CROSS_COMPILE}" menuconfig
+
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make ARCH="${ARCH}" CROSS_COMPILE="${CROSS_COMPILE}" defconfig
+make ARCH="${ARCH}" CROSS_COMPILE="${CROSS_COMPILE}" -j$(nproc)
+make ARCH="${ARCH}" CROSS_COMPILE="${CROSS_COMPILE}" CONFIG_PREFIX="${OUTDIR}/rootfs" install
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+LIBS=($(ldd "${OUTDIR}/rootfs/bin/busybox" | awk '{print $3}' | grep -v "^$"))
+for lib in "${LIBS[@]}"; do
+    cp -L "$lib" "${OUTDIR}/rootfs/lib/"
+done
 
 # TODO: Make device nodes
+sudo mknod -m 666 "${OUTDIR}/rootfs/dev/null" c 1 3
+sudo mknod -m 666 "${OUTDIR}/rootfs/dev/tty" c 5 0
+sudo mknod -m 666 "${OUTDIR}/rootfs/dev/zero" c 1 5
 
 # TODO: Clean and build the writer utility
+cd "${FINDER_APP_DIR}"
+make clean
+make
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp "${FINDER_APP_DIR}/finder.sh" "${OUTDIR}/rootfs/home/"
+cp "${FINDER_APP_DIR}/conf/username.txt" "${OUTDIR}/rootfs/home/"
+cp "${FINDER_APP_DIR}/conf/assignment.txt" "${OUTDIR}/rootfs/home/"
+cp "${FINDER_APP_DIR}/finder-test.sh" "${OUTDIR}/rootfs/home/"
+cp "${FINDER_APP_DIR}/autorun-qemu.sh" "${OUTDIR}/rootfs/home/"
 
 # TODO: Chown the root directory
+sudo chown -R root:root "${OUTDIR}/rootfs"
 
 # TODO: Create initramfs.cpio.gz
+cd "${OUTDIR}/rootfs"
+find . | cpio --quiet -o -H newc | gzip -9 > "${OUTDIR}/initramfs.cpio.gz"
